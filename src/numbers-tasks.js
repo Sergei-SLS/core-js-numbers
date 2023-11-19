5679415b66abe053b33e3670655971e7ab0014e7
Sergei-SLS <lyamin.sls@gmail.com>
2023-11-19 21:13:57 +0600

    docs: done task 2

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -34,8 +34,10 @@ function getRectangleArea(width, height) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  const pi = Math.PI;
+  const circle = 2 * pi * radius;
+  return circle;
 }
 
 /**
@@ -50,8 +52,9 @@ function getCircleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  const average = (value1 + value2) / 2;
+  return average;
 }
 
 /**
